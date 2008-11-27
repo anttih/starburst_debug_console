ae556000984773a054900a15ffae81379df400c5
Antti Holvikari <anttih@gmail.com>
2008-11-27 22:14:11 +0200

    Added a locale file for Finnish. Just a placeholder.

---
+++ Starburst/Debug/Console/Locale/fi_FI.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+    'TEXT_SOLAR_SQL'         => 'SQL Profiler',
+    'TEXT_SOLAR_LOG'         => 'Logs',
+    'TEXT_SOLAR_REQUEST'     => 'Environment',
+    'TEXT_SOLAR_DEBUG_TIMER' => 'Timer',
+);
